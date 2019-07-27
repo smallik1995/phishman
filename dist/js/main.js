e4895c03d7c6ec7ff61c183559bfd9981ebf8778
smallik1995 <smallik666@outlook.com>
2019-07-27 10:43:36 +0300

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,19 @@
+var div_menu = document.getElementsByClassName('navbar-container');
+window.onscroll = function (e) {
+  console.log('123');
+  if (div_menu) {
+    div_menu.style.top = window.pageYOffset + 'px';
+    console.log('321');
+  }
+}
 
+window.onscroll = function() {
+  console.log('135');
+}
+
+alert('123')
+
+
+// $(window).scroll(function() {
+//   console.log('sdfsf')
+// })
